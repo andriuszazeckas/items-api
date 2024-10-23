7be5deaab0678843f44b003baf9927ca54798b46
andriuszazeckas <andrius.zazeckas@vinted.com>
2024-10-23 17:50:47 +0300

    refactored items_controller to more clean look

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,16 +4,9 @@ require_relative '../domain/items/filter'
 
 class ItemsController < ApplicationController
   def show
-    @item = Item.find_by(uuid: params[:uuid])
+    @item = Item.find_by!(uuid: params[:uuid])
 
-    render json: {
-      uuid: @item.uuid,
-      price: @item.price,
-      brand: @item.brand,
-      photo_url: @item.photo_url,
-      status: @item.status,
-      user_uuid: @item.uuid,
-    }
+    render json: @item.slice(:uuid, :price, :brand, :photo_url, :status, :user_id)
   end
 
   def index
@@ -21,56 +14,38 @@ class ItemsController < ApplicationController
     per_page = 2
     paginate_items = filtered_items.page(params[:page]).per(per_page)
 
-    items_output(paginate_items)
-
-    # render json: {
-    #   items: filtered_items.map { |item| { id: item.id, brand: item.brand, status: item.status } },
-    # }
-  end
-
-  # def index
-  #   brand_param = params[:brand]
-  #   status_param = params[:status]
-  #
-  #   filter = Items::Filter.new(brand_param, status_param)
-  #   filtered_items = filter.run
-  #   items_output(filtered_items)
-  # end
-
-  def items_output(items = nil?)
-    render json: {
-      items: items.map do |item|
-        { uuid: item.uuid, brand: item.brand, status: item.status, price: item.price,
-          photo_url: item.photo_url, user_id: item.user_id }
-      end,
-    }
+    render json: { items: paginate_items.map do |item|
+      item.slice(:uuid, :price, :brand, :photo_url, :status, :user_id)
+    end }
   end
 
   def create
     @item = Item.new(item_params)
+
     if @item.save
-      render json: { message: 'Item successfully updated!' }, status: :created
+      render json: { message: 'Item successfully created!' }, status: :created
     else
-      render json: { errors: @item.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: @item.errors }, status: :unprocessable_entity
     end
   end
 
   def update
-    @item = Item.find_by(uuid: params[:uuid])
+    @item = Item.find_by!(uuid: params[:uuid])
+
     if @item.update(item_params)
       render json: { message: 'Item successfully updated!' }, status: :ok
-
     else
-      render json: { errors: @item.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: @item.errors }, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @item = Item.find_by(uuid: params[:uuid])
+    @item = Item.find_by!(uuid: params[:uuid])
+
     if @item.destroy
       render json: { message: 'Item successfully deleted!' }, status: :ok
     else
-      render json: { errors: @item.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: @item.errors }, status: :unprocessable_entity
     end
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Item < ApplicationRecord
-
   before_create :set_uuid
 
   private
