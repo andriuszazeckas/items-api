b2a74659f05ebaf7dd7f660a0d177617a2b3bafd
andriuszazeckas <andrius.zazeckas@vinted.com>
2024-10-24 09:03:07 +0300

    Update filter.rb

--- app/domain/items/filter.rb
+++ app/domain/items/filter.rb
@@ -14,23 +14,3 @@ module Items
     end
   end
 end
-
-# OLD CODE:
-
-# # frozen_string_literal: true
-#
-# module Items
-#   class Filter
-#     def initialize(brand = nil, status = nil)
-#       @brand = brand
-#       @status = status
-#     end
-#
-#     def run
-#       items = Item.all
-#       items = items.where(brand: @brand) if @brand.present?
-#       items = items.where(status: @status) if @status.present?
-#       items
-#     end
-#   end
-# end
